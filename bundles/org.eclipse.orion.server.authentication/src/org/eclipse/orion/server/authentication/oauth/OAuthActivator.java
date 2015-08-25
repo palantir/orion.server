2516367d23659a5bdfc2373d845363909aa9c2fd
Michal Wlodarczyk <mwlodarczyk@palantir.com>
2015-08-25 13:34:43 -0700

    Bug 474348 Authentication logic is now divided into bundles

---
+++ bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/oauth/OAuthActivator.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corporation and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.orion.server.authentication.oauth;
+
+/**
+ * An abstract class registering services of an authorization plug-in.
+ * New plug-in's Activator class should override getOAuthParamsFactory()
+ * method returning specific OAuthParamsFactory object which is
+ * being used for creating OAuthParams.
+ *
+ * @author mwlodarczyk
+ *
+ */
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public abstract class OAuthActivator implements BundleActivator {
+	ServiceRegistration oauthServiceRegistration;
+
+	public void start(BundleContext context) throws Exception {
+		OAuthParamsFactory oauthParamsFactory = getOAuthParamsFactory();
+
+		Hashtable<String, String> properties = new Hashtable<String, String>();
+		properties.put("provider", oauthParamsFactory.getOAuthProviderName());
+
+		oauthServiceRegistration = context.registerService(OAuthParamsFactory.class.getName(),
+				oauthParamsFactory, properties);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		oauthServiceRegistration.unregister();
+	}
+
+	protected abstract OAuthParamsFactory getOAuthParamsFactory();
+}

86de4c0c04d8ddfc5bea25d801089c7564ef2392
Michal Wlodarczyk <mwlodarczyk@palantir.com>
2015-10-29 12:49:26 -0700

    ORION-96 redirecting from OAuth provider supports context path
    Change-Id: I59eac7f65b412ccb7d2594d15f857762b5515aec

--- bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/oauth/OAuthParams.java
+++ bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/oauth/OAuthParams.java
@@ -19,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest.AuthenticationRequestBuilder;
 import org.apache.oltu.oauth2.client.response.OAuthAccessTokenResponse;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.eclipse.orion.server.core.PreferenceHelper;
+import org.eclipse.orion.server.core.ServerConstants;
 
 /**
  * A set of abstract methods used to store information for OAuth token providers
@@ -79,7 +81,7 @@ public abstract class OAuthParams {
 
 	protected URL getCurrentURL() throws OAuthException {
 		try {
-			return new URL(currentURL.getProtocol(), currentURL.getHost(), currentURL.getPort(), "");
+			return new URL(currentURL.getProtocol(), currentURL.getHost(), currentURL.getPort(), PreferenceHelper.getString(ServerConstants.CONFIG_CONTEXT_PATH, ""));
 		} catch (MalformedURLException e) {
 			throw new OAuthException("An error occured while authenticating");
 		}
